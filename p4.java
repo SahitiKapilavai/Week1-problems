1d19f8ed5c8f233cf0eaf506e2bee2ab2f47fe87
SahitiKapilavai <49801996+SahitiKapilavai@users.noreply.github.com>
2020-03-14 07:37:18 +0530

    Add files via upload

---
+++ p4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class A{  
+int result(int n) 
+{ if (n == 1) 
+       return 0; 
+  else if (n % 2 == 0) 
+       return (result(n/2)+1); 
+  else
+       return (1+Math.min(result(n - 1),result(n + 1))); 
+} 
+public static void main(String args[]) 
+{  Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+A a=new A();
+  System.out.println(a.result(n)); 
+  sc.close();
+} 
+} 
